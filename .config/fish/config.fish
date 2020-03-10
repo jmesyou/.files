7b6d7e548ea82d4e1edf3c2ad4cc1240815a9397
James You <james.you@protonmail.com>
2020-03-09 23:17:59 -0400

    switch shells to xonsh

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-set PATH /var/lib/snapd/snap/bin $PATH
-set PATH ~/.local/bin $PATH
-set XDG_DATA_DIRS /var/lib/snapd/desktop/:$XDG_DATA_DIRS
-
-alias ls="exa"
-alias emacs="emacs -nw"
-alias please="sudo"
-alias copy="xclip -sel clip"
