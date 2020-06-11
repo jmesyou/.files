2761f60bb753ea8ecfa030287f4a4fd8a3531c33
James You <james.you@protonmail.com>
2020-06-11 01:10:30 -0400

    add new prompt + new git workflow helpers

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,15 @@
-function please --description 'alias please=sudo'
-  sudo $argv
+# Setup Fisher by default
+if test -e $HOME/.config/fish/functions/fisher.fish
+else 
+    curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
 end
 
+# Aliases 
+alias please='sudo'
 alias emacs='emacs -nw'
 alias ls='exa'
 
+# Dot file configurator
 function dotfiles --description 'dot file configuration'
     git --git-dir=$HOME/.files/ --work-tree=$HOME $argv
 end
